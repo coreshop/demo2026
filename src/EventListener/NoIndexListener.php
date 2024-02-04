e1e02b5d7eb320893c65e47e6d32667aa836f97e
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2024-02-04 19:51:07 +0100

    add no-index response header

---
+++ src/EventListener/NoIndexListener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class NoIndexListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.response' => 'onKernelResponse',
+        ];
+    }
+
+    public function onKernelResponse($event)
+    {
+        $response = $event->getResponse();
+        $response->headers->set('X-Robots-Tag', 'noindex follow');
+    }
+}
