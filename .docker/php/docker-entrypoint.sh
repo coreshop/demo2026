b84880932768defcfaab7fb79aaa3ffdea60adb8
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2024-06-11 16:53:03 +0200

    fix install priority

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -8,11 +8,11 @@ fi
 
 /usr/local/bin/wait_db
 /usr/local/bin/install
+bin/console coreshop:install
+COMPOSER_MEMORY_LIMIT=-1 composer dump-autoload --classmap-authoritative --optimize
 bin/console pimcore:bundle:install PimcoreStudioBackendBundle
 bin/console pimcore:bundle:install PimcoreGenericDataIndexBundle
 bin/console pimcore:bundle:install PimcoreApplicationLoggerBundle
-bin/console coreshop:install
-COMPOSER_MEMORY_LIMIT=-1 composer dump-autoload --classmap-authoritative --optimize
 bin/console coreshop:install:demo
 
 exec docker-php-entrypoint "$@"
