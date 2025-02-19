e85328469eae3b190c96619913c301aa67a7994f
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2025-02-19 08:21:40 +0100

    updates

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
+    Pimcore\Bundle\QuillBundle\PimcoreQuillBundle::class => ['all' => true],
     Pimcore\Bundle\SeoBundle\PimcoreSeoBundle::class => ['all' => true],
     Pimcore\Bundle\CustomReportsBundle\PimcoreCustomReportsBundle::class => ['all' => true],
     Pimcore\Bundle\NewsletterBundle\PimcoreNewsletterBundle::class => ['all' => true],
