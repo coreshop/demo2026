f710e4f1f9be3df3fcf2e6c7af8b09d5dff91e89
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2025-07-24 08:53:28 +0200

    add install keys

--- .docker/php/docker-install.sh
+++ .docker/php/docker-install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+export PIMCORE_INSTALL_ENCRYPTION_SECRET=***REMOVED***
+export PIMCORE_INSTALL_INSTANCE_IDENTIFIER=***REMOVED***
+export PIMCORE_INSTALL_PRODUCT_KEY=***REMOVED***
+
 echo "Install Pimcore"
 vendor/bin/pimcore-install --skip-database-config --no-interaction
 
